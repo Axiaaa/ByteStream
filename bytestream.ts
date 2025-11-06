079f1d21af1b9f304ee7c8f4254ddd98977d3cfa
Nephtys <91245292+Axiaaa@users.noreply.github.com>
2025-11-06 11:58:00 +0100

    Implement ByteStream class for binary data handling

---
+++ bytestream.ts
@@ -0,0 +1,77 @@
+class ByteStream {
+
+    buffer : number[] = []
+    offset = 0
+    bitoffset = 0  
+
+
+  constructor() {
+    this.buffer = []
+    this.offset = 0
+    this.bitoffset = 0  
+  }
+  
+  print() {
+    const binaryString = Array.from(this.buffer)
+      .map(b => `0b${b.toString(2).padStart(8, '0')}`)
+      .join(' ');
+    console.log(binaryString)
+  }
+  
+  reset(val1?: number, val2?: number) {
+    this.offset = (val1 === undefined ? 0 : val1)
+    this.bitoffset = (val2 === undefined ? 0 : val2)
+  }
+  
+  readBool() {
+    const val = (this.buffer[this.offset] >> this.bitoffset) & 1;
+    this.bitoffset = (this.bitoffset + 1) & 7;
+    if (this.bitoffset === 0) this.offset++;
+    return val !== 0;
+  }
+  
+  writeBool(value: boolean) {
+    if (this.bitoffset == 0) { 
+        this.buffer.push(0);  
+        this.offset++;
+    }
+    if (value) {
+        this.buffer[this.offset - 1] |= 1 << (this.bitoffset & 7);
+    }
+    this.bitoffset = (this.bitoffset + 1) & 7;
+  }
+  
+  readInt() {
+    this.bitoffset = 0;
+    let nb = 
+        (this.buffer[this.offset] << 24) |
+        (this.buffer[this.offset + 1] << 16) |
+        (this.buffer[this.offset + 2] << 8) |
+        this.buffer[this.offset + 3];
+    this.offset += 4;
+    return nb;
+  }
+  
+  writeInt(value : number) {
+    this.bitoffset = 0
+    this.buffer.push((value >> 24) & 0xFF);
+    this.buffer.push((value >> 16) & 0xFF);
+    this.buffer.push((value >> 8) & 0xFF);
+    this.buffer.push(value & 0xFF);  
+    this.offset += 4;
+  }
+}
+
+let bs = new ByteStream()
+
+bs.writeBool(true)
+bs.writeBool(true)
+bs.writeBool(true)
+bs.writeBool(true)
+console.log(bs.readBool())
+console.log(bs.readBool())
+console.log(bs.readBool())
+console.log(bs.readBool())
+bs.writeInt(305419896)
+bs.reset(1)
+console.log(bs.readInt())
